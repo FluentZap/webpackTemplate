d84d6d1f5fc7d8e573b646e39efcac0973ec651a
Todd Aden <FluentZap@Gmail.com>
2019-05-29 23:26:14 -0700

    remove karma

--- test/main.test.js
+++ test/main.test.js
@@ -1,5 +1,9 @@
 import { add } from '../src/functions';
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+    expect(add(1, 2)).toBe(3);    
+});
+
+test('adds 5 + 5 to equal 10', () => {
+    expect(add(5, 5)).toBe(10);
 });
